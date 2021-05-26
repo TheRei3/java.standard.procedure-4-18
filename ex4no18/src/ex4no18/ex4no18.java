bca7303a70acd84069115def9bd132afd460d806
admin <admin@DESKTOP-8L2IG92>
2021-05-27 07:00:36 +0900

    ex4no18

---
+++ ex4no18/src/ex4no18/ex4no18.java
@@ -0,0 +1,12 @@
+package ex4no18;
+
+public class ex4no18 { // 163p
+	public static void main(String[] args) {
+		for( int i = 2 ; i <= 9 ; i++) {
+			for( int j = 1 ; j <= 9 ; j++) {
+				System.out.printf("%d x %d = %d%n", i, j, i*j);
+			}
+		}
+	}
+
+}
